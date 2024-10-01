1998587729197b3191067c4108960d27705d611c
Danaraujoc <daniel.araujox77@gmail.com>
2024-10-01 19:57:41 -0300

    created avengers api first version

---
+++ src/test/kotlin/com/dio/avengers/AvengersApiApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.dio.avengers
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class AvengersApiApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
