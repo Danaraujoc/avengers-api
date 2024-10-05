631e085abab0d59def1e015fbfd94e49f116e186
Danaraujoc <daniel.araujox77@gmail.com>
2024-10-04 21:54:16 -0300

    added files to attend avenger resource

---
+++ src/main/kotlin/com/dio/avengers/application/web/resource/response/AvengerResponse.kt
@@ -0,0 +1,23 @@
+package com.dio.avengers.application.web.resource.response
+
+import com.dio.avengers.domain.avenger.Avenger
+
+data class AvengerResponse(
+
+    val id : Long?,
+    val nick: String,
+    val person: String,
+    val description: String?,
+    val history: String?
+){
+    companion object{
+        fun from (avenger:Avenger) = AvengerResponse(
+            id = avenger.id,
+            nick = avenger.nick,
+            person = avenger.person,
+            description = avenger.description,
+            history = avenger.history,
+        )
+    }
+}
+
